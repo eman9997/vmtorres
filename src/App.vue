dc82c454bf48c4fe31c4e1609aadcbe461857c0d
Emanuel Fonseca <emanuelfonseca@Emanuels-MBP-2.lan>
2023-08-06 15:24:07 -0700

    inital init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Dashboard from '@/components/Dashboard.vue'
+</script>
+
+<template>
+  <Dashboard msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container-fluid">
+    <Navbar />
+    <img src="../../public/dash.jpg" alt="dash">
+
+    <div id="app">
+      <VCarousel :items="carouselItems" />
+    </div>
+  </div>
+  <!-- <h1>{{ msg }}</h1> -->
+
+  
+</template>
+
+<script setup lang="ts">
+import Navbar from './Navbar.vue'
+import VCarousel from './VCarousel.vue'
+import { ref } from 'vue';
+
+const carouselItems = ref([
+  'Slide 1',
+  'Slide 2',
+  'Slide 3',
+  'Slide 4',
+  'Slide 5',
+]);
+</script>
+
+<style scoped lang="scss">
+.container-fluid {
+  width: 100%;
+  height: 100vh;
+  background: $primary;
+}
+
+.read-the-docs {
+  color: #888;
+}
+</style>
