48de0bdf5dbdf5a9ee538b344e67ab6e6c1a5d0f
Emanuel Fonseca <emanuelfonseca@Emanuels-MBP-2.lan>
2023-12-13 16:34:26 -0800

    debugging

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const app = express();
 // Serve static files
 app.use(express.static(__dirname + '/vmtorres'));
 
-app.get('/api', function(req, res) {
-  res.send('API works');
-
-});
+// app.get('/api', function(req, res) {
+//   res.send('API works');
+// });
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/vmtorres/index.html'));
 });
 
