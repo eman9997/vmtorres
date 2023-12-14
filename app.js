67a5beee4f609fc2572c91ce68ed761d96422ff8
Emanuel Fonseca <emanuelfonseca@Emanuels-MBP-2.lan>
2023-12-13 16:43:08 -0800

    debugging

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/vmtorres'));
+app.use(express.static(__dirname + 'vmtorres'));
 
 // app.get('/api', function(req, res) {
 //   res.send('API works');
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/vmtorres'));
 
 // Send all requests to index.html
 app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname + '/vmtorres/index.html'));
+  res.sendFile(path.join(__dirname + 'vmtorres/index.html'));
 });
 
 // Default Heroku PORT
